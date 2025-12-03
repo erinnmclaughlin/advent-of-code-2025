98ee13e62b6ff3bed0ffa0da0e0139d995c5a3b4
Erin McLaughlin <sa-em5mb@myuva.onmicrosoft.com>
2025-12-03 00:39:48 -0500

    day 3

---
+++ AdventOfCode2025/Day03/Solver.cs
@@ -0,0 +1,43 @@
+namespace AdventOfCode2025.Day03;
+
+public sealed class Solver : ISolver<Solver>
+{
+    public static int DayNumber => 3;
+    
+    public (string? PartOne, string? PartTwo) Solve(FileInfo inputFile)
+    {
+        long partOne = 0; 
+        long partTwo = 0;
+        
+        foreach (var line in File.ReadLines(inputFile.FullName))
+        {
+            var digits = line.Select((c, i) => new Digit(int.Parse(c.ToString()), i)).ToList();
+            partOne += Solve(digits, 2);
+            partTwo += Solve(digits, 12);
+        }
+
+        return (partOne.ToString(), partTwo.ToString());
+    }
+
+    private static long Solve(List<Digit> digits, int targetDigitCount)
+    {
+        var selectedDigits = new List<int>();
+        var startingPosition = 0;
+
+        while (selectedDigits.Count < targetDigitCount)
+        {
+            var remainingDigitCount = targetDigitCount - selectedDigits.Count;
+                
+            var max = digits.Where(x => x.Position >= startingPosition && x.Position < digits.Count - remainingDigitCount + 1).MaxBy(x => x.Value);
+
+            if (max == null)
+                break;
+
+            startingPosition = max.Position + 1;
+            selectedDigits.Add(max.Value);
+        }
+
+        return long.Parse(string.Join("", selectedDigits));
+    }
+    private sealed record Digit(int Value, int Position);
+}
