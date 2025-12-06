c62fa23c456648e183e91f2128b4b60bc74d0bfa
Erin McLaughlin <erinmclaughlin219@gmail.com>
2025-12-05 21:58:11 -0500

    day 1 fsharp

--- fsharp/day01.fsx
+++ fsharp/day01.fsx
@@ -1,33 +1,41 @@
 open System.IO
 
-let processLine (currentPos: int) (line: string) : int * int =
-    let increment = if line[0] = 'L' then -1 else 1
-    [ 1 .. line[1..] |> int ]
-    |> List.fold 
-       (
-           fun (cur, pass) _ ->
-           let next = (currentPos + increment + 100) % 100
-           let pass' = if next = 0 then pass + 1 else pass
-           next, pass'
-       )
-       (currentPos, 0)
+let parseInstruction (instruction: string) =
+    let ticks = instruction[1..] |> int
+    if instruction[0] = 'L' then -ticks else ticks
+
+let toDialPos rawValue =
+    let m = rawValue % 100
+    if m < 0 then m + 100 else m
+
+let getDiv a = if a >= 0 then a / 100 else (a - 99) / 100
+
+let solve (pos, partOne, partTwo) instruction =
+    let delta = parseInstruction instruction
+    let nextPosRaw = pos + delta
+    let nextPos = toDialPos nextPosRaw
+
+    let partOneIncrement = if nextPos = 0 then 1 else 0
+    let partTwoIncrement =
+        if delta > 0 then
+            getDiv nextPosRaw
+        elif delta < 0 then
+            getDiv (pos - 1) - getDiv (nextPosRaw - 1)
+        else
+            0
+
+    nextPos,
+    partOne + partOneIncrement,
+    partTwo + partTwoIncrement
 
 let filePath =
     match fsi.CommandLineArgs |> Array.skip 1 with
     | [| fp |] -> fp
-    | _ -> System.IO.Path.Combine("..", "inputs", "day01.txt")
+    | _ -> Path.Combine("inputs", "day01.txt")
 
-printfn "%s" filePath
+let fileLines = File.ReadLines filePath
 
-let _, exactlyZero, passThroughZero = 
-    File.ReadLines filePath
-    |> Seq.fold
-        (fun (dialPos, exactCount, passCount) line ->
-            let dialPosAfter, passDelta = processLine dialPos line
-            let exactCountDelta = if dialPosAfter = 0 then 1 else 0
-            dialPosAfter, exactCount + exactCountDelta, passCount + passDelta
-        )
-        (50, 0, 0)
-
-printfn "Part one: %i" exactlyZero
-printfn "Part two: %i" passThroughZero
+fileLines
+    |> Seq.fold solve (50, 0, 0)
+    |> fun (_, partOne, partTwo) ->
+        printfn "part one: %i\npart two: %i" partOne partTwo
